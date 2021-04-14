6e35e929f200a2208e5f1b060b6ba62010b2ddaf
Jim Wardlaw <jim.wardlaw@gmail.com>
2021-04-14 16:29:08 +0100

    Refactor (link) update to glimmer component

--- portal/files/__root__/components/ui/link.js
+++ portal/files/__root__/components/ui/link.js
@@ -1,21 +1,17 @@
-import Component from '@ember/component';
-import {inject as service} from '@ember/service';
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
 
 export default class UiLinkComponent extends Component {
-  // Properties
-
-  tagName = '';
-
   // Service
 
   @service router;
 
   // Getters
 
-  isActive() {
-    const thisRoute = this.route + '',
-      currentRoute = this.router.currentRoute.name + '',
-      activeWhen = this.activeWhen ? this.activeWhen.split(',') : [];
+  get isActive() {
+    const thisRoute = this.route + "",
+      currentRoute = this.router.currentRoute.name + "",
+      activeWhen = this.activeWhen ? this.activeWhen.split(",") : [];
 
     activeWhen.pushObject(thisRoute);
 
